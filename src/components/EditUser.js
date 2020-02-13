3d333639edad7bcf3dbdbc4222fa937f84b129c5
Adrian <adrian.belver@etservices.eu>
2020-02-13 15:40:48 +0100

    Homework done and corrected

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -12,11 +12,16 @@ class EditUser extends React.Component {
             userName: "",
             email: ""
         }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleUserNameChange = this.handleUserNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.editUser = this.editUser.bind(this);
+        this.cancelButton = this.cancelButton.bind(this);
     }
 
     componentDidMount() {
         let filteredList = this.context.listOfUsers.filter(user => (user.id == this.context.userId));
-        filteredList.length > 0 && this.setState({name: filteredList[0].name, userName:filteredList[0].username, email: filteredList[0].email });
+        filteredList.length > 0 && this.setState({ name: filteredList[0].name, userName: filteredList[0].username, email: filteredList[0].email });
 
     }
 
@@ -24,12 +29,78 @@ class EditUser extends React.Component {
 
     }
 
+    handleNameChange(event) {
+        this.setState({ name: event.target.value });
+    }
+
+    handleUserNameChange(event) {
+        this.setState({ userName: event.target.value });
+    }
+
+    handleEmailChange(event) {
+        this.setState({ email: event.target.value });
+    }
+
+
+    editUser(event) {
+        event.preventDefault();
+        fetch('https://jsonplaceholder.typicode.com/users/' + this.context.userId, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name: this.state.name,
+                username: this.state.userName,
+                email: this.state.email
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(response => alert(JSON.stringify(response)));
+    }
+
+    cancelButton() {
+        this.props.history.push("/users");
+    }
+
+
     render() {
-        return (<div>Welcome to Edit User
-            <h6>{this.state.name}</h6>
-            <h6>{this.state.userName}</h6>
-            <h6>{this.state.email}</h6>
-        </div>);
+        return (
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 col-xl-9 mx-auto">
+                        <div className="card card-signin flex-row my-5">
+                            <div className="card-img-left d-none d-md-flex">
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Edit User</h5>
+                                <form className="form-signin">
+                                    <div className="form-label-group">
+                                        <input type="text" id="inputUserName" className="form-control" value={this.state.userName} onChange={this.handleUserNameChange} placeholder="Username" required autofocus />
+                                        <label for="inputUserame">User Name</label>
+                                    </div>
+
+                                    <div className="form-label-group">
+                                        <input type="text" id="inputName" className="form-control" value={this.state.name} onChange={this.handleNameChange} placeholder="Name" required />
+                                        <label for="inputName">User</label>
+                                    </div>
+                                    <div className="form-label-group">
+                                        <input type="text" id="inputEmail" className="form-control" value={this.state.email} onChange={this.handleEmailChange} placeholder="Email" required />
+                                        <label for="inputEmail">Email</label>
+                                    </div>
+                                    <button type="button" class="btn btn-primary btn-lg" type="submit" onClick={this.editUser} disabled={this.state.submitDisabled}>Edit user</button>
+                                    <div className="float-right">
+                                        <button type="button" class="btn btn-secondary btn-lg btn-secondary" type="submit" onClick={this.cancelButton}>Cancel</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
     }
 }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -6,69 +6,114 @@ import Topbar from "./Topbar";
 import EditUser from "./EditUser";
 
 class Users extends React.Component {
-    static contextType = AppContext;
+  static contextType = AppContext;
 
-    constructor(props) {
-        super(props);
-        this.state = {listofUserPosts:[]}//{listOfUsers: []}
+  constructor(props) {
+    super(props);
+    this.state = {
+      listofUserPosts: [],
+      postsByUser: []
     }
+    this.showUserPosts = this.showUserPosts.bind(this);
+    this.goToEditUser = this.goToEditUser.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
 
-    componentDidMount() {
-        !this.context.userLogged && this.props.history.push("/");
-        this.retrieveUsers();
-        this.retrieveUserPosts();
-        this.showUserPosts = this.showUserPosts.bind(this);
-        this.goToEditUser = this.goToEditUser.bind(this);
-    }
 
-    retrieveUsers(){
+  }
+
+  componentDidMount() {
+    !this.context.userLogged && this.props.history.push("/");
+    this.retrieveUsers();
+    this.retrieveUserPosts();
+  }
+
+  retrieveUsers() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(res => res.json())
       .then((json) => {
         //this.setState({ listOfUsers: json })
         this.context.setListOfUsers([...json]);
-        this.context.setOriginalUsers([...json]);       
+        this.context.setOriginalUsers([...json]);
       })
-    }
+  }
 
-    retrieveUserPosts(){
-        fetch("https://jsonplaceholder.typicode.com/posts")
-          .then(res => res.json())
-          .then((json) => {
-            this.setState({ listofUserPosts: json })
-          })
-        }
-
-    showUserPosts(event){
-        //alert("the id is "+ event.target.id);
-        this.state.listofUserPosts.filter(post => (post.userId == event.target.id) && alert(post.body));
-    }
+  retrieveUserPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({ listofUserPosts: json })
+      })
+  }
 
 
-    goToEditUser(event, id){
-      this.context.setUserId(id);
-      this.props.history.push("/edituser");
-    }
+  showUserPosts(id) {
+    let userPostsById = this.state.listofUserPosts.filter(post => (post.userId == id)); 
+    this.setState({ postsByUser: userPostsById });
+  }
 
-    render() {
-        return <div>
-            <Navbar />
-            <Topbar />
-            <h6>This is the list of users:</h6>
-             
-            {this.context.listOfUsers.map((user, index) => 
-            
-            <div onClick = {this.showUserPosts}>{index}. {user.name}-{user.address.geo.lat}
-              <button style = {{float: "right"}} type="button" className="btn btn-primary tiny" onClick = {event=>this.goToEditUser(event, user.id)}>Edit</button>
-              <br/><br/></div>
-              
-              )}
-            
-            
-        </div>
-    }
+  goToEditUser(event, id) {
+    this.context.setUserId(id);
+    this.props.history.push("/edituser");
+  }
 
-    //ol: ordered list (números) envuelve a li: list item
+  deleteUser(event, id) {
+    event.preventDefault();
+    fetch('http://jsonplaceholder.typicode.com/users/' + id, {
+      method: 'DELETE'
+    }).then( response => alert("User deleted"))
+    .catch(error=>alert("Sorry, there was an internal error: " + error));
+  }
+
+  render() {
+    return <div>
+      <Navbar />
+      <Topbar />
+      <h6>This is the list of users:</h6>
+
+      {this.context.listOfUsers.map((user) =>
+
+        <div>{user.id}. {user.name}-{user.address.geo.lat}
+
+          <div>
+
+            <button style={{ float: "left" }} className="btn btn-outline-success" data-toggle="modal" data-target={"#modal" + user.id} onClick={() => this.showUserPosts(user.id)}>{user.username}'s posts</button>
+
+
+            <div className="modal fade" id={"modal"+ user.id} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLongTitle">{user.username}'s posts</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+
+                    {this.state.postsByUser.map(post => <div>{post.id}.-{post.body}</div>)}
+
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <button style={{ float: "right" }} type="button" className="btn btn-danger tiny" onClick={event => this.deleteUser(event, user.id)}>Delete</button>
+          <button style={{ float: "right" }} type="button" className="btn btn-primary tiny" onClick={event => this.goToEditUser(event, user.id)}>Edit</button>
+
+          <br /><br /></div>
+
+      )}
+
+
+    </div>
+  }
+
+  //ol: ordered list (números) envuelve a li: list item
 
 }
 
